9dd5b07ce8a92cf7aee8c660262dd4d310e9b9f3
jpanoccam <jpanoccam@unsa.edu.pe>
2025-05-28 22:48:28 -0500

    implementacion de vista de alex

--- asistiot/lib/main.dart
+++ asistiot/lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'views/home_view.dart';
+import 'package:provider/provider.dart';
+
+import '../viewmodels/device_viewmodel.dart';
+import '../views/device_list_view.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => DeviceViewModel()..loadDevices(),
       child: MaterialApp(
-        home: HomeView(),
+        home: DeviceListView(),
       ),
-    ),
-  );
+    );
+  }
 }
---
+++ asistiot/lib/models/device_model.dart
@@ -0,0 +1,6 @@
+class Device {
+  final String name;
+  final String ip;
+
+  Device({required this.name, required this.ip});
+}
